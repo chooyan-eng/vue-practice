9a000387eab458bcdf875ddec6a40854fed29890
chooyan-eng <chooyan.engineer@gmail.com>
2018-02-13 14:42:56 +0900

    [modify] make articles data array and render with v-for

--- articles.js
+++ articles.js
@@ -1,8 +1,14 @@
-var article = {title: "What is Vue.js?", body: "Vue.js is The Progressive JavaScript Framework"}
+var articles = [
+    {title: "What is Vue.js?", body: "Vue.js is The Progressive JavaScript Framework"},
+    {title: "Approachable", body: "Already know HTML, CSS and JavaScript? Read the guide and start building things in no time!"},
+    {title: "Versatile", body: "An incrementally adoptable ecosystem that scales between a library and a full-featured framework."},
+]
 
 function init() {
     var articleList = new Vue({
         el: '#list',
-        data: article
+        data: {
+            articles: articles
+        }
     });
 }
