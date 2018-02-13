274ec11f14d8ebe36e28996828a3c25d2412ca11
chooyan-eng <chooyan.engineer@gmail.com>
2018-02-13 14:39:55 +0900

    [modify] use template and data bindings for the article list

--- articles.js
+++ articles.js
@@ -1,3 +1,8 @@
+var article = {title: "What is Vue.js?", body: "Vue.js is The Progressive JavaScript Framework"}
+
 function init() {
-    
+    var articleList = new Vue({
+        el: '#list',
+        data: article
+    });
 }
